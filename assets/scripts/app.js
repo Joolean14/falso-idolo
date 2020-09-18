7a110e6c5512e9fff5e26c90849f7038c8811f72
Julian Loaiza <you@example.com>
2020-09-18 18:26:55 -0500

    Compressed images and began checking out swiper js.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,12 +1,22 @@
-let swipeRight = document.querySelector(".swipe-right");
-let swipeLeft = document.querySelector(".swipe-left")
-
-// swipeLeft.addEventListener("click", swipingRight);
-swipeRight.addEventListener("click", swipingRight);
-
-function swipingRight () {
-    let section = document.createElement("div");                 // Create a <li> node
-    let textnode = document.createTextNode("Water");         // Create a text node
-    section.appendChild(textnode);                              // Append the text to <li>
-    document.getElementById("main-container").appendChild(div);     // Append <li> to <ul> with id="myList" 
-}
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false, 
+    mousewheel: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  })
