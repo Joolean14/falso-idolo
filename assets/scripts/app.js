daf1a9737884e46ea7b206bc693c28beafb38130
julian <Joolean14>
2020-11-13 15:17:32 -0500

    Cambio velocidad swiper

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,7 @@ var swiper = new Swiper('.swiper-container', {
       slidesPerView: 1,
       spaceBetween: 30,
       loop: true,
+      speed: 1800,
       loopFillGroupWithBlank: true,
       navigation: {
         nextEl: '.swiper-button-next',
